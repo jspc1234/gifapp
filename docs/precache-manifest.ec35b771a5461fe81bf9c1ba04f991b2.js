3e819075fe09fb3c3eba05dc8f0a477462f69b97
Julian peña <sebas.456@hotmail.com>
2020-08-11 21:05:51 -0500

    Initial commit

---
+++ docs/precache-manifest.ec35b771a5461fe81bf9c1ba04f991b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9513277f51f7e61e199370d27077d53e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1694d6dfd8b490aa98f",
+    "url": "/static/css/main.ece7ac5a.chunk.css"
+  },
+  {
+    "revision": "5a7778c249de4a249b53",
+    "url": "/static/js/2.c18cb374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c18cb374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1694d6dfd8b490aa98f",
+    "url": "/static/js/main.a596c18c.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
